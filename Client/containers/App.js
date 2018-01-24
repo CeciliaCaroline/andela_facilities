6942639cc17762e9e8a9756c74d59f7e8438c6ec
ceciliacaroline <nalubegac58@gmail.com>
2018-01-24 10:39:15 +0300

    Add react client setup with webpack

---
+++ Client/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './login';
+// import Logout from './logout';
+
+const App = () => {
+    return (
+    <div>
+         <Login />
+         {/* <Logout /> */}
+    </div>
+    )
+}
+
+export default App
