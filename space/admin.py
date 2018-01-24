423830b824f713fd23855845bd26ed433880bd19
ceciliacaroline <nalubegac58@gmail.com>
2018-01-24 15:07:55 +0300

    Register user model

--- space/admin.py
+++ space/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from space.models import Space, Room, Occupant
+from accounts.models import User
+
 
 # Register your models here.
 admin.site.register(Space)
 admin.site.register(Room)
 admin.site.register(Occupant)
+admin.site.register(User)
