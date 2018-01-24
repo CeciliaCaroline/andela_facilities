2d059bb9c8f6ed970983b6a4a8afc0719832d5f0
ceciliacaroline <nalubegac58@gmail.com>
2018-01-24 10:36:39 +0300

    Add token handling urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path, include
-from .views import RegisterView
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_jwt.views import (refresh_jwt_token, verify_jwt_token,
+                                      obtain_jwt_token)
+from .views import RegisterView, GoogleUserView
 
 
 urlpatterns = [
-    path('auth/login', RegisterView.as_view(), name='googleUser'),
+    path('user/', GoogleUserView.as_view(), name='app_user'),
+    path('auth/', include([
+        path('login', RegisterView.as_view(), name='googleUser'),
+        path('refresh-token/', refresh_jwt_token, name='refresh token'),
+        path('verify-token/', verify_jwt_token, name='verify token'),
+        path('obtain-token/', obtain_jwt_token, name='obtain token'),
+    ]))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
