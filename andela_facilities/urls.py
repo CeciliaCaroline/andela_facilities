1c3e8785e2457e7e77d9e1f6caff68b1738b7e7d
ceciliacaroline <nalubegac58@gmail.com>
2018-01-24 10:37:13 +0300

    Remove token urls

--- andela_facilities/urls.py
+++ andela_facilities/urls.py
@@ -1,17 +1,15 @@
 from django.urls import include, path
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.contrib.auth import views as auth_views
-from rest_framework_jwt.views import (refresh_jwt_token, verify_jwt_token,
-                                      obtain_jwt_token)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', auth_views.login),
     path('accounts/', include('accounts.urls')),
-    path('refresh-token/', refresh_jwt_token, name='refresh token'),
-    path('verify-token/', verify_jwt_token, name='verify token'),
-    path('obtain-token/', obtain_jwt_token, name='obtain token'),
     path('spaces/', include('space.urls')),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
