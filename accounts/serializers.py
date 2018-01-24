b3016da6b6ae37f05892f06df379cbb7df5811ba
ceciliacaroline <nalubegac58@gmail.com>
2018-01-24 10:21:41 +0300

    Add User serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = User
+        fields = ('id', 'username', 'appuser_picture')
